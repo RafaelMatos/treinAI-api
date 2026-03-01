06863a3ef5f10c9af782e1f1e0f4a0852f9dfa0f
Rafael Matos <rafael-matos@outlook.com>
2026-03-01 09:28:16 -0300

    feat: add fastify api

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
-const name = "";
+import "dotenv/config"
 
-console.log( "Hello World!");
+import Fastify from 'fastify'
+const fastify = Fastify({
+    logger: true
+})
+
+
+fastify.get('/', async function handler() {
+    return { hello: 'world' }
+})
+
+
+try {
+    await fastify.listen({ port: Number(process.env.PORT) || 8081 })
+} catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+}
